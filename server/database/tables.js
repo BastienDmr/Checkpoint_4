77b909bbddde1d67870b48acf6d385005fda7de7
BastienDmr <domer.bastien@gmail.com>
2024-08-22 10:05:58 +0200

    Mise en place de la table projet

--- server/database/tables.js
+++ server/database/tables.js
@@ -2,6 +2,7 @@
 const ItemRepository = require("./models/ItemRepository");
 const ProfilRepository = require("./models/ProfilRepository");
 const SkillRepository = require("./models/SkillRepository");
+const ProjectRepository = require("./models/SkillRepository");
 
 // Create an empty object to hold data repositories for different tables
 const tables = {};
@@ -14,6 +15,7 @@ const tables = {};
 tables.item = new ItemRepository();
 tables.profil = new ProfilRepository();
 tables.skill = new SkillRepository();
+tables.project = new ProjectRepository();
 
 /* ************************************************************************* */
 
